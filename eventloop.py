cf7a34c91219911b1368f58bca20298ce40de215
Dai Kieu <42823136+daikieu95@users.noreply.github.com>
2018-10-10 21:07:55 -0700

    Add files via upload

---
+++ eventloop.py
@@ -0,0 +1,26 @@
+"""
+CPSC 386 - Project 4
+Create a Pacman Portal Game
+Name: DAI KIEU
+"""
+import pygame
+from game import run_game
+
+
+def eventLoop(message,left,all_sprites_list,block_list,monsta_list,pacman_collide,wall_list,gate):
+  while True:
+      # ALL EVENT PROCESSING SHOULD GO BELOW THIS COMMENT
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+        if event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_ESCAPE:
+            pygame.quit()
+          if event.key == pygame.K_RETURN:
+            del all_sprites_list
+            del block_list
+            del monsta_list
+            del pacman_collide
+            del wall_list
+            del gate
+            run_game()
